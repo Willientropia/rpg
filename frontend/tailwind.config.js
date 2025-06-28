a98b49f863cea7f94c0c081cf114d8d67fa64bf8
Willientropia <willie.engenharia@gmail.com>
2025-06-28 17:49:25 -0300

    tudo funcionando primeira vez

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,20 +7,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        // Paleta D&D
         primary: {
-          50: '#fdf8f0',
           500: '#8B4513',
           600: '#704010',
-          700: '#5a320d',
         },
         secondary: {
           500: '#DAA520',
-          600: '#b8891a',
-        },
-        accent: {
-          500: '#DC143C',
-          600: '#b30f2f',
         }
       }
     },
